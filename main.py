6a41acf23440ab28cc2520b738d8e5747174b102
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-24 20:16:09 -0300

    v0.1

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import math
+
+# Variaveis ------------------------------
+rosa = '#e2979c'
+vermelho = '#e7305b'
+verde = '#9bdeac'
+amarelo = '#f7f5dd'
+fonte = 'Courier'
+minutos_trabalho = 25
+pausa_pequena = 5
+pausa_longa = 20
+repticoes = 0
+timer = None
+
